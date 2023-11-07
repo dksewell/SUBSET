097c55069b08295cffd532f399d789b47b739d91
Sewell <dksewell@uiowa.edu>
2023-11-07 14:12:29 -0600

    Created/modified fixed IS algo to be correct.  It also uses Bayes factors to determine nu if not provided.

--- R/SUBSET_IS_fixed.R
+++ R/SUBSET_IS_fixed.R
@@ -19,17 +19,14 @@
 #' *Note that that the parameter space must be convex.* (It usually is.)
 #' 
 #' 
-#' @param draws0 matrix. The posterior draws  
-#' corresponding to the base prior.
+#' @param draws0 named list with names 'posterior' and 'prior'. 
+#' Each ought to be a matrix of posterior or prior draws (each row is a draw),
+#' all of which should correspond to the base prior.
 #' @param Proj_mat matrix.  Projection matrix for the 
 #' desired linear subspace you wish to shrink towards.
-#' @param v_sequence vector of positive values. See details.
 #' @param CI_level numeric between (0,1). Level of the 
 #' posterior credible interval.
-#' @param min_ESS integer.  Minimum number of effective sample size. WARNING:
-#' Don't make this too high, or you risk not representing the full parameter space well.
-#' @param n_u_values Number of values to try for u, the hyperparameter of the 
-#' proposal distribution that shrinks towards the linear subspace.
+#' @param min_ESS integer.  Minimum number of effective sample size. 
 #' @param verbose logical. Should any output be provided?
 #' 
 #' @return Object of class "subset_fixed", "subset_IS_fixed", with named elements summary 
@@ -72,144 +69,205 @@
 #' @export
 
 
+if(TRUE){
+  library(SUBSET)
+  library(Matrix)
+  library(dplyr)
+  library(mvtnorm)
+  library(parallel)
+  library(splines)
+  
+  set.seed(2023)
+  theta_true = c(1,1.5)
+  N = 50
+  ndraws = 1e4
+  # y ~ N(mu, I_2)
+  y = mvtnorm::rmvnorm(N,theta_true,diag(2))
+  # mu ~ N(0,2.5^2 * I_2)
+  prior_cov = 2.5^2 * diag(2)
+  # Draw from base prior
+  prior_draws = 
+    mvtnorm::rmvnorm(ndraws, numeric(2), 2.5^2 * diag(2))
+  # Draw from posterior (under base prior)
+  post_cov = 
+    chol2inv(chol( N * diag(2) + chol2inv(chol(prior_cov)) ))
+  post_mean = 
+    post_cov %*% diag(2) %*% (N * colMeans(y))
+  posterior_draws = 
+    mvtnorm::rmvnorm(ndraws, post_mean, post_cov)
+  
+  draws0 = 
+    list(prior = prior_draws,
+         posterior = posterior_draws)
+  
+  Proj_mat = Proj(c(1,1))
+  CI_level = 0.95
+  min_ESS = 1/10 * nrow(draws0$posterior)
+  verbose = TRUE
+  
+  cl = makeCluster(10)
+  
+}
+
 SUBSET_IS_fixed = function(draws0,
-                           Proj_mat = Proj(matrix(1,ncol(Sigma0),1)),
-                           v_sequence = seq(0.25,5,by = 0.25),
+                           Proj_mat = Proj(matrix(1,ncol(draws0),1)),
                            CI_level = 0.95,
-                           min_ESS = 1/2 * nrow(draws0),
-                           n_u_values = 100,
-                           verbose = TRUE){
-  ndraws = nrow(draws0)
+                           min_ESS = 1/10 * nrow(draws0),
+                           verbose = TRUE,
+                           nu,
+                           cl){
+  ndraws = sapply(draws0,nrow)
   CI_level = 1 - CI_level
-  p = ncol(draws0)
-  v_len = length(v_sequence)
+  p = ncol(draws0$posterior)
   I_m_P = diag(p) - Proj_mat
+  if(verbose)if(missing(nu))cat("\nNo nu provided.  Picking level of shrinkage via Bayes Factor.\n")
   
-  TT = function(nu){
-    sapply(1:ndraws,
-           function(i){
-             exp(-0.5 * nu * tcrossprod(draws0[i,],draws0[i,] %*% I_m_P))
-           })
-  }
+  if(!missing(cl)) if(is.numeric(cl)) cl = makeCluster(min(cl,detectCores() - 1))
   
-  ESS = function(u,Tnu){
-    w = Tnu^(u^2)
-    w = w / sum(w)
-    1 / sum(w^2)
-  }
   
-  draws_u = results = list()
-  
-  for(v in 1:v_len){
-    if(verbose){
-      cat("\n")
-      cat(paste(rep("-",4 + nchar(as.character(v_sequence[v]))),collapse=""))
-      cat("\n")
-      cat(paste0("v = ",v_sequence[v]))
-      cat("\n")
-      cat(paste(rep("-",4 + nchar(as.character(v_sequence[v]))),collapse=""))
-      cat("\n")
-    }
-    
-    if(!is.null(colnames(draws0))){
-      results[[v]] = 
-        data.frame(variable = colnames(draws0),
-                   mean = numeric(p),
-                   lower = numeric(p),
-                   upper = numeric(p),
-                   v = v_sequence[v])
-    }else{
-      results[[v]] = 
-        data.frame(variable = paste0("v",1:p),
-                   mean = numeric(p),
-                   lower = numeric(p),
-                   upper = numeric(p),
-                   v = v_sequence[v])
-    }
-    
-    Tnu = TT(v_sequence[v])
-    
-    u_seq = 
-      seq(1,0,l = n_u_values+1)[-(n_u_values+1)]
-    for(uu in u_seq){
-      ESS_value = 
-        ESS(uu,Tnu)
-      if(ESS_value > min_ESS){
-        u = uu
-        break
-      }
-    }
-    results[[v]]$ESS = ESS_value
-    results[[v]]$u = u
-    
-    # ESS_values = 
-    #   sapply(seq(0,1,l = n_u_values+1)[-1],ESS,Tnu = Tnu)
-    # u = seq(0,1,l=n_u_values)[max(which(ESS_values > min_ESS))]
-    # results[[v]]$ESS = ESS_values[max(which(ESS_values > min_ESS))]
-    # results[[v]]$u = u
-    
-    draws_u[[v]] = 
-      cbind(u * draws0 + (1 - u) * draws0 %*% Proj_mat,
-            weight = Tnu / sum(Tnu))
-    if(!is.null(colnames(draws0))) colnames(draws_u[[v]])[1:p] = colnames(draws0)
-    
-    results[[v]]$mean = 
-      apply(draws_u[[v]][,1:p], 2, weighted.mean, w = draws_u[[v]][,"weight"])
-    results[[v]]$mean = 
-      apply(draws_u[[v]][,1:p], 2, weighted.mean, w = draws_u[[v]][,"weight"])
-    
-    for(j in 1:p){
-      x = draws_u[[v]][,j]
-      ord = order(x)
-      w = draws_u[[v]][ord,"weight"]
-      x = x[ord]
-      rw <- cumsum(w)
-      selection <- min(which(rw >= CI_level/2))
-      if (rw[selection] == CI_level/2){
-        results[[v]]$lower[j] = mean(x[selection:(selection + 1)])
-      }else{
-        results[[v]]$lower[j] = x[selection]
-      }
-      selection <- min(which(rw >= 1 - CI_level/2))
-      if (rw[selection] == 1 - CI_level/2){
-        results[[v]]$upper[j] = mean(x[selection:(selection + 1)])
-      }else{
-        results[[v]]$upper[j] = x[selection]
-      }
-    }
-    
-  }
-  names(draws_u) = v_sequence
-  
-  results = do.call(rbind,results)
-  
-  if(!is.null(colnames(draws0))){
-    results = 
-      rbind(data.frame(variable = colnames(draws0),
-                       mean = colMeans(draws0),
-                       lower = apply(draws0,2,quantile,prob = CI_level/2),
-                       upper = apply(draws0,2,quantile,prob = 1 - CI_level/2),
-                       v = 0.0,
-                       ESS = nrow(draws0),
-                       u = NA),
-            results)
+  # Find optimal nu
+  ## Compute w_k(1)
+  if(missing(cl)){
+    w_1 = 
+      sapply(1:ndraws["posterior"],
+             function(i){
+               exp(-0.5 * tcrossprod(draws0$posterior[i,],draws0$posterior[i,] %*% I_m_P))
+             })
+    wtilde_1 = 
+      sapply(1:ndraws["prior"],
+             function(i){
+               exp(-0.5 * tcrossprod(draws0$prior[i,],draws0$prior[i,] %*% I_m_P))
+             })
   }else{
-    results = 
-      rbind(data.frame(variable = paste0("v",1:ncol(draws0)),
-                       mean = colMeans(draws0),
-                       lower = apply(draws0,2,quantile,prob = CI_level/2),
-                       upper = apply(draws0,2,quantile,prob = 1 - CI_level/2),
-                       v = 0.0,
-                       ESS = nrow(draws0),
-                       u = NA),
-            results)
+    clusterExport(cl,c("draws0","I_m_P"),envir = environment())
+    w_1 = 
+      sapply(1:ndraws,
+             function(i){
+               exp(-0.5 * tcrossprod(draws0$posterior[i,],draws0$posterior[i,] %*% I_m_P))
+             })
+    wtilde_1 = 
+      sapply(1:ndraws,
+             function(i){
+               exp(-0.5 * tcrossprod(draws0$prior[i,],draws0$prior[i,] %*% I_m_P))
+             })
   }
-  colnames(results)[3:4] = 
+  
+  ## Create function that creates w_k(\nu)
+  if(missing(cl)){
+    get_w_k = function(nu){
+      w_1^nu
+    }
+    get_wtilde_k = function(nu){
+      wtilde_1^nu
+    }
+  }else{
+    clusterExport(cl,c("w_1","wtilde_1"),envir = environment())
+    get_w_k = function(nu){
+      parSapply(cl,w_1,function(x)x^nu)
+    }
+    get_wtilde_k = function(nu){
+      parSapply(cl,wtilde_1,function(x)x^nu)
+    }
+  }
+  
+  if(missing(nu)){
+    ## Find maximum \nu allowed by ESS
+    find_max_nu = function(nu){
+      w_k = get_w_k(nu)
+      w_k = w_k / sum(w_k)
+      
+      ESS = 1 / sum(w_k^2)
+      
+      (ESS - min_ESS)^2
+    }
+    nu_max = nu_upper_bound = 1e3
+    safety = 0
+    if(verbose) cat("\n---Finding maximum nu to satisfy ESS constraints\n")
+    while( ( abs(nu_max - nu_upper_bound) / nu_upper_bound < 1e-3) & safety < 100){
+      nu_upper_bound = 2 * nu_upper_bound
+      nu_max = 
+        optimize(find_max_nu,
+                 interval = c(0,nu_upper_bound))$min
+      safety = safety + 1
+    }
+    
+    
+    ## Get optimal nu according to bayes factor
+    best_nu = function(nu){
+      # Give log( BF(0,nu) )
+      log(mean(get_wtilde_k(nu))) - 
+        log(mean(get_w_k(nu)))
+    }
+    nu = optimize(best_nu,
+                  interval = c(0,nu_max))$min
+  }
+  
+  if(!missing(cl)) clusterExport(cl,"nu",envir=environment())
+  
+  # Get final IS weights
+  w_k = get_w_k(nu)
+  BF_nu_0 = 
+    exp(
+      log(mean(get_w_k(nu))) - 
+        log(mean(get_wtilde_k(nu)))
+    )
+  w_k = w_k / sum(w_k)
+  
+  # Get ESS
+  ESS = 1 / sum(w_k^2)
+  
+  # Get point estimates
+  theta_hat = 
+    apply(draws0$posterior,2,weighted.mean,w = w_k)
+  
+  # Get CI
+  get_CI_from_IS = function(x,w){
+    ord = order(x)
+    w = w[ord]
+    x = x[ord]
+    w_cum = cumsum(w)
+    
+    c(lower = x[min(which(w_cum >= CI_level/2))],
+      upper = x[min(which(w_cum >= 1 - CI_level/2))]
+    )
+  }
+  
+  # Put results together
+  if(is.null(colnames(draws0$posterior))){
+    if(!is.null(colnames(draws0$prior))){
+      colnames(draws0$posterior) = 
+        colnames(draws0$prior)
+    }else{
+      colnames(draws0$posterior) = 
+        colnames(draws0$prior) = 
+        paste("theta",1:p,sep="_")
+    }
+  }
+  results = list()
+  results$summary = 
+    data.frame(variable = colnames(draws0$posterior),
+               posterior_mean = theta_hat,
+               lower = numeric(p),
+               upper = numeric(p))
+  for(j in 1:p){
+    temp = 
+      get_CI_from_IS(draws0$posterior[,j],w_k)
+    results$summary$lower[j] = temp["lower"]
+    results$summary$upper[j] = temp["upper"]
+  }
+  colnames(results$summary)[3:4] = 
     c(paste0(CI_level/2 * 100,"%"),
       paste0((1-CI_level/2) * 100,"%"))
-  results = list(summary = results,
-                 is_draws = draws_u)
+  
+  results$nu = nu
+  results$ESS = ESS
+  results$BF_favoring_nu = BF_nu_0
+  results$proposal_draws = draws0$posterior
+  results$is_weights = w_k
+  results$base_prior_draws = draws0$prior
+  
   class(results) = c("subset_fixed","subset_IS_fixed")
+  
   rownames(results$summary) = NULL
   return(results)
 }
